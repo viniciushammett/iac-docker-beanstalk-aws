5e0ca48ecd18fe88b54184febc91e6130b864e5d
viniciushammett <vincius_te@hotmail.com>
2022-10-04 00:03:44 -0300

    updating infra

---
+++ infra/role.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "beanstalk_ec2" {
+  name = "beanstalk-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  }
+
+  resource "aws_iam_role_policy" "beanstalk_ec2_policy" {
+  name = "beanstalk-ec2-policy"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus",
+          "logs:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
