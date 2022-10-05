50b7ec21b3e5acdd242a796aea43da9168776e77
viniciushammett <vincius_te@hotmail.com>
2022-10-04 22:45:52 -0300

    building app v2

--- infra/beanstalk.tf
+++ infra/beanstalk.tf
@@ -2,3 +2,9 @@ resource "aws_elastic_beanstalk_application" "aplicacao_beanstalk" {
   name        = var.nome
   description = var.descricao
 }
+
+resource "aws_elastic_beanstalk_environment" "ambiente_beanstalk" {
+  name                = var.ambiente
+  application         = aws_elastic_beanstalk_application.aplicacao_beanstalk.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.5.0 running Docker"
+}
